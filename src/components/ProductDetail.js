bdadd3e37af363274815d02ec2497963127904c1
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-09 18:27:27 +0200

    add handleModalContentClick

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,31 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ProductDetail(props) {
+    const handleModalContentClick = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <div className="modal">
-            <div className="modal__dialog">
-                <div className="modal__content">
-                    <header className="modal__header">
-                        <h2 className="modal__title">Camiseta: {props.name}</h2>
-                        <a href="/">
-                            <span className="modal__close icon fas fa-times"></span>
-                        </a>
-                    </header>
-                    <section>
-                        <img
-                            className="card__img"
-                            src={props.imageUrl}
-                            alt={props.name}
-                        />
-                        <ul className="ml-1 mt-1">
-                            <li>Precio: {props.precio}</li>
-                            <li>Descripción: {props.description}</li>
-                            <li>Tallas: {props.sizes.join('/')}</li>
-                        </ul>
-                    </section>
+        <Link to="/">
+            <div className="modal">
+                <div className="modal__dialog">
+                    <div
+                        className="modal__content"
+                        onClick={handleModalContentClick}
+                    >
+                        <header className="modal__header">
+                            <h2 className="modal__title">
+                                Camiseta: {props.name}
+                            </h2>
+                            <Link to="/">
+                                <span className="modal__close icon fas fa-times"></span>
+                            </Link>
+                        </header>
+                        <section>
+                            <img
+                                className="card__img"
+                                src={props.imageUrl}
+                                alt={props.name}
+                            />
+                            <ul className="ml-1 mt-1">
+                                <li>Precio: {props.precio}</li>
+                                <li>Descripción: {props.description}</li>
+                                <li>Tallas: {props.sizes.join('/')}</li>
+                            </ul>
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
 
